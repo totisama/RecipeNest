da709c0b7c36359ebcce80d934d37a0e073e3275
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-20 22:45:48 +0100

    fix typo

--- database/migrations/2024_11_19_094951_create_ingredient_steps_table.php
+++ database/migrations/2024_11_19_094951_create_ingredient_steps_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('ingredient_steps', function (Blueprint $table) {
+        Schema::create('ingredient_step', function (Blueprint $table) {
             $table->foreignId('step_id')->constrained()->onDelete('cascade');
             $table->foreignId('ingredient_id')->constrained()->onDelete('cascade');
             $table->float('amount');
