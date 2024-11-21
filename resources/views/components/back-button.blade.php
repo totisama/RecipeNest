ae0baef6b857db8a85f3fd6a9b172d8fffcbb519
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-21 22:24:30 +0100

    Add BackButton component

---
+++ resources/views/components/back-button.blade.php
@@ -0,0 +1,8 @@
+<a href="{{ url()->previous() }}"
+    class="inline-flex items-center gap-2 px-4 py-2 bg-[#5B3A1F] text-white text-sm font-medium rounded-lg shadow-md hover:bg-[#412913] transition-colors duration-300">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+        stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+    </svg>
+    Back
+</a>
