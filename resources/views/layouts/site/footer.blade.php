5e6ce303120328d83af31a385bc16ba51c173985
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-22 10:22:44 +0100

    small responsive change

--- resources/views/layouts/site/footer.blade.php
+++ resources/views/layouts/site/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-gray-200 py-4 border-black border-t-[2px]">
+<footer class="bg-gray-200 px-5 py-4 border-black border-t-[2px] lg:px-0">
     <div class="container mx-auto flex items-center justify-between">
         <div class="w-1/4">
             <img src="{{asset('images/recipeNest-logo.webp')}}" alt="Recipe Nest Logo" class="w-12 h-12" />
