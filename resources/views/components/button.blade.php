7e3a1bc8e599fbea0b6d3b1a69833aa06b36f17b
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-22 10:22:28 +0100

    Add button component

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,17 @@
+@php
+    $baseClasses = 'inline-block px-6 py-3 text-base font-medium rounded-lg shadow-md transition-all ease-out duration-300
+    hover:scale-105';
+    $primaryClasses = 'bg-[#5B3A1F] text-white hover:bg-[#412913] hover:shadow-lg';
+    $secondaryClasses = 'bg-gray-200 text-[#5B3A1F] hover:bg-gray-300 hover:shadow-lg';
+    $deleteClasses = 'bg-red-500 text-white hover:bg-red-600 hover:shadow-lg';
+
+    $classes = match ($mode) {
+        'secondary' => "$baseClasses $secondaryClasses",
+        'delete' => "$baseClasses $deleteClasses",
+        'primary' => "$baseClasses $primaryClasses"
+    };
+@endphp
+
+<button type="{{ $type }}" {{ $attributes->merge(['class' => $classes]) }}>
+    {{ $slot }}
+</button>
