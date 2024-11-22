af4dc3cb16c65689f8907a537b320b1c9a476504
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-22 12:19:46 +0100

    Modify Link and Button component style

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,5 +1,5 @@
 @php
-    $baseClasses = 'inline-block px-6 py-3 text-base font-medium rounded-lg shadow-md transition-all ease-out duration-300
+    $baseClasses = 'inline-block px-3 py-1 text-base font-medium rounded-lg shadow-md transition-all ease-out duration-300
     hover:scale-105';
     $primaryClasses = 'bg-[#5B3A1F] text-white hover:bg-[#412913] hover:shadow-lg';
     $secondaryClasses = 'bg-gray-200 text-[#5B3A1F] hover:bg-gray-300 hover:shadow-lg';
