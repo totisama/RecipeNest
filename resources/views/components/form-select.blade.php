fee8d93a6cb5a2a3a53d92003f8a65f496b5a5ed
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-26 00:22:53 +0100

    Changes to display value and label keys

--- resources/views/components/form-select.blade.php
+++ resources/views/components/form-select.blade.php
@@ -4,7 +4,9 @@
         class="p-1 rounded-lg border border-gray-300 @error($name) border-red-500 @enderror"">
         <option value="">Select an option</option>
         @foreach ($options as $option)
-            <option value="{{ $option }}" {{ $option === $value ? 'selected' : '' }}>{{ $option }}</option>
+                <option value=" {{ $option['value'] }}" {{ $option['value'] === $value ? 'selected' : '' }}>
+            {{ $option['label'] }}
+            </option>
         @endforeach
     </select>
     @error($name) <span class="text-red-600">{{ $message }}</span>
