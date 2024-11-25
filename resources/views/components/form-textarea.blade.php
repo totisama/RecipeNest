f66065879ae1776e8b9e095b233e5d812ad3e777
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-25 12:04:30 +0100

    fix textarea component

--- resources/views/components/form-textarea.blade.php
+++ resources/views/components/form-textarea.blade.php
@@ -1,6 +1,6 @@
-<div class="my-5 flex flex-col">
-  <label class="block text-xs font-semibold uppercase" for="content">Content</label>
-  <textarea class="p-1 rounded-lg border border-gray-300 @error('content') border-red-500 @enderror"
-    name="content">{{old('content')}}</textarea>
-  @error('content') <span class="text-red-600">{{$message}}</span> @enderror
+<div class="flex flex-col gap-1">
+    <label class="block text-xs font-semibold uppercase" for="{{ $name }}">{{$label}}</label>
+    <textarea class="p-1 rounded-lg border border-gray-300 @error($name) border-red-500 @enderror" id="{{ $name }}"
+        name="{{ $name }}">{{old($name)}}</textarea>
+    @error($name) <span class="text-red-600">{{$message}}</span> @enderror
 </div>
