3db6e6d1477b0c5e05212a45a874b6dfaf15f551
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-26 23:11:53 +0100

    change color of secondary mode

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,7 +2,7 @@
     $baseClasses = 'inline-block px-3 py-1 text-base font-medium rounded-lg shadow-md transition-all ease-out duration-300
     hover:scale-105';
     $primaryClasses = 'bg-[#5B3A1F] text-white hover:bg-[#412913] hover:shadow-lg';
-    $secondaryClasses = 'bg-gray-200 text-[#5B3A1F] hover:bg-gray-300 hover:shadow-lg';
+    $secondaryClasses = 'bg-white border-[1px] border-gray-400 text-[#5B3A1F] hover:bg-gray-100 hover:shadow-lg';
     $deleteClasses = 'bg-red-500 text-white hover:bg-red-600 hover:shadow-lg';
 
     $classes = match ($mode) {
