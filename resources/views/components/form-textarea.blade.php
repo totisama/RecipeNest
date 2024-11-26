bd9279c8e6daa3b317510dbdbadefef3c3271b01
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-26 17:09:11 +0100

    add missing default value

--- resources/views/components/form-textarea.blade.php
+++ resources/views/components/form-textarea.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col gap-1">
     <label class="block text-xs font-semibold uppercase" for="{{ $name }}">{{$label}}</label>
     <textarea class="p-1 rounded-lg border border-gray-300 @error($name) border-red-500 @enderror" id="{{ $name }}"
-        name="{{ $name }}">{{old($name)}}</textarea>
+        name="{{ $name }}">{{old($name, $value)}}</textarea>
     @error($name) <span class="text-red-600">{{$message}}</span> @enderror
 </div>
