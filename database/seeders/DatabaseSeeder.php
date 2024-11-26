b1d91dd5b2de0a7c500d9af6fec81e2012f0597a
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-26 23:19:24 +0100

    changes to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,31 +22,31 @@ class DatabaseSeeder extends Seeder
             'password' => '$2y$12$nJT5MdPhGzDqNZFfbjxCL.gROd37N0Y/f6mSx7dxV67Uju5Jtza/S',
         ]);
 
-        $users = User::factory(1)->create();
+        // $users = User::factory(1)->create();
 
-        $users->prepend($user);
+        // $users->prepend($user);
 
-        $recipes = Recipe::factory(10)->create([
-            'user_id' => fn () => $users->random()->id,
-        ]);
+        // $recipes = Recipe::factory(10)->create([
+        //     'user_id' => fn () => $users->random()->id,
+        // ]);
 
         $ingredients = Ingredient::factory(50)->create();
 
-        $recipes->each(function ($recipe) use ($ingredients) {
-            $steps = Step::factory(rand(3, 8))->create([
-                'recipe_id' => $recipe->id,
-            ]);
+        // $recipes->each(function ($recipe) use ($ingredients) {
+        //     $steps = Step::factory(rand(3, 8))->create([
+        //         'recipe_id' => $recipe->id,
+        //     ]);
 
-            $steps->each(function ($step) use ($ingredients) {
-                $randomIngredients = $ingredients->random(rand(2, 5));
+        //     $steps->each(function ($step) use ($ingredients) {
+        //         $randomIngredients = $ingredients->random(rand(2, 5));
 
-                foreach ($randomIngredients as $ingredient) {
-                    $step->ingredients()->attach($ingredient->id, [
-                        'amount' => fake()->randomFloat(2, 0.1, 5), // Random amount for each ingredient
-                        'unit' => fake()->randomElement(['grams', 'kilograms', 'liters', 'milliliters', 'pounds', 'ounces']),
-                    ]);
-                }
-            });
-        });
+        //         foreach ($randomIngredients as $ingredient) {
+        //             $step->ingredients()->attach($ingredient->id, [
+        //                 'amount' => fake()->randomFloat(2, 0.1, 5), // Random amount for each ingredient
+        //                 'unit' => fake()->randomElement(['grams', 'kilograms', 'liters', 'milliliters', 'pounds', 'ounces']),
+        //             ]);
+        //         }
+        //     });
+        // });
     }
 }
