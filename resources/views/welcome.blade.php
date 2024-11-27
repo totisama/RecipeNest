2261234f286105e036decec9f10314311ecaf86a
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-11-27 12:07:55 +0100

    tweak responsive of welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,16 +6,16 @@
         class="px-5 mt-3 mb-10 grid gap-x-8 gap-y-6 place-items-center">
         @foreach ($recipes as $recipe)
             <a href="{{route('recipes.show', [$recipe->id])}}"
-                class='group h-72 overflow-hidden select-none max-w-80 pb-3 flex flex-col items-center gap-4 bg-white w-full px-5 rounded-3xl transition-transform duration-300 ease-out md:h-full hover:cursor-pointer hover:scale-105'>
+                class='group overflow-hidden select-none max-w-80 pb-3 flex flex-col items-center gap-4 bg-white w-full px-5 rounded-3xl transition-transform duration-300 ease-out h-full hover:cursor-pointer'>
                 <img src="{{$recipe->image}}" alt="{{$recipe->title}} image"
-                    class="rounded-3xl h-48 transition-transform duration-300 ease-out md:h-56 group-hover:scale-110" />
+                    class="rounded-3xl h-48 transition-transform duration-500 ease-in-out md:h-56 group-hover:scale-105" />
                 <div class="h-full w-full flex flex-col justify-between">
                     <h2 class="text-[#5B3A1F] text-center font-bold text-xl md:text-2xl">{{ $recipe->title }}</h2>
                     <div class="px-3 flex flex-col">
-                        <small class="text-semibold text-[#B2794B] text-base md:text-lg">
+                        <small class="text-semibold text-[#B2794B] text-lg">
                             {{count($recipe->steps)}} steps
                         </small>
-                        <small class="text-semibold text-[#B2794B] text-base md:text-lg">
+                        <small class="text-semibold text-[#B2794B] text-lg">
                             Estimated time: {{$recipe->formatTime()}}
                         </small>
                     </div>
