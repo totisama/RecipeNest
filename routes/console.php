ba9d9d1d1eb853ccadbc0e59b9ad8a69d04fb3ba
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-01-23 20:34:11 +0100

    backups config

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command('backup:clean')->dailyAt('03:10')->timezone('Europe/Madrid');
+Schedule::command('backup:run --only-db')->dailyAt('03:15')->timezone('Europe/Madrid');
+Schedule::command('backup:run')->sundays()->at('03:20')->timezone('Europe/Madrid');
